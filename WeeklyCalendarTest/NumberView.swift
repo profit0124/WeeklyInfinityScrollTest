92e11e1a4459f2b6198db09bcbfbfeeebd34eb87
profit0124 <profit0124@gmail.com>
2023-02-27 21:22:56 +0900

    first commit

---
+++ WeeklyCalendarTest/NumberView.swift
@@ -0,0 +1,20 @@
+//
+//  NumberView.swift
+//  WeeklyCalendarTest
+//
+//  Created by Sooik Kim on 2023/02/27.
+//
+
+import SwiftUI
+
+struct NumberView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NumberView_Previews: PreviewProvider {
+    static var previews: some View {
+        NumberView()
+    }
+}
